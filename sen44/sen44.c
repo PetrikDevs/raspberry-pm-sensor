c610c45549e790567212a091ae30ccd19a2f35a2
Csongor Kertesi <48154331+csokertesi@users.noreply.github.com>
2023-11-22 09:15:23 +0000

    Refactor measure function signature

--- sen44/sen44.c
+++ sen44/sen44.c
@@ -30,28 +30,12 @@ void sleep(int ms)
 	sensirion_uart_hal_sleep_usec((uint32_t)ms * (uint32_t)1000);
 }
 
-// TODO: direct?
 int measure(int* masscon_pm1p0, int* masscon_pm2p5, int* masscon_pm4p0, int* masscon_pm10p0, float* voc_index, float* ambient_humidity, float* ambient_temp)
 {
-	uint16_t _masscon_pm1p0;
-	uint16_t _masscon_pm2p5;
-	uint16_t _masscon_pm4p0;
-	uint16_t _masscon_pm10p0;
-	float _voc_index;
-	float _ambient_humidity;
-	float _ambient_temp;
 	int16_t error = sen44_read_measured_mass_concentration_and_ambient_values(
-		&_masscon_pm1p0, &_masscon_pm2p5, &_masscon_pm4p0, &_masscon_pm10p0,
-		&_voc_index, &_ambient_humidity, &_ambient_temp
+		&masscon_pm1p0, &masscon_pm2p5, &masscon_pm4p0, &masscon_pm10p0,
+		&voc_index, &ambient_humidity, &ambient_temp
 	);
-
-	*masscon_pm1p0 = _masscon_pm1p0;
-	*masscon_pm2p5 = _masscon_pm2p5;
-	*masscon_pm4p0 = _masscon_pm4p0;
-	*masscon_pm10p0 = _masscon_pm10p0;
-	*voc_index = _voc_index;
-	*ambient_humidity = _ambient_humidity;
-	*ambient_temp = _ambient_temp;
 
 	return (int)error;
 }
